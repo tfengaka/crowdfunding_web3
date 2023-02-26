510d61ef2d3519bcfb5a41f61685dbe2a14d6777
tfengaka <nthoa2.lhp.nh@gmail.com>
2023-02-27 06:33:54 +0700

    setup base frontend

--- contractions/hardhat.config.js
+++ contractions/hardhat.config.js
@@ -1,12 +1,21 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
 module.exports = {
-  solidity: {
-    version: '0.8.9',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+	solidity: {
+		version: "0.8.9",
+		defaultNewtwork: "goerli",
+		networks: {
+			hardhat: {},
+			bsc_testnet: {
+				url: "https://data-seed-prebsc-1-s2.binance.org:8545",
+				accounts: [process.env.PRIVATE_KEY],
+			},
+		},
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
 };
